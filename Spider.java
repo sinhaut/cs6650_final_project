af9a75387c71ae715bba4a285cb8440fa8450526
Nolan <braman.n@northeastern.edu>
2022-04-25 18:06:14 -0700

    basic spider implemented

--- Spider.java
+++ Spider.java
@@ -1,26 +1,54 @@
+import org.jsoup.HttpStatusException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-
 import java.io.IOException;
-import java.rmi.RemoteException;
 
+/**
+ * Spider is a class that takes a given URL, and returns that URL with either a string http response,
+ * or a string error code. */
 public class Spider {
-
     private String result;
     private String currUrl;
-    private String errorCode;
 
     public Spider() {
+        // Seems a bit redundant if no data is saved between goes.
+        // Maybe just have a main method and call from there?
     }
 
+
+    public String request() {
+        // Request a new URL from the dispatcher.
+        // feed it into the crawl func.
+        // probably have a loop in main.
+        return "";
+    }
+
+    /**
+     * Crawls a given url. If the response is valid, it returns an array
+     * in the format of
+     * [String url, String http].
+     * If it's not valid, it returns
+     * [String url, String error code int]
+     */
     public String[] crawl(String url) throws IOException {
-        Document doc = Jsoup.connect(url).get();
-        System.out.println(doc.text());
-        return new String[] {this.currUrl, this.result};
+        try {
+            Document doc = Jsoup.connect(url).get();
+            System.out.println(doc.text());
+            this.result = doc.html();
+        } catch (HttpStatusException ex) {
+            // return the url and the status code as a string.
+            return new String[]{this.currUrl, Integer.toString(ex.getStatusCode())};
+        }
+        return new String[]{this.currUrl, this.result};
     }
 
-     public static void main(String[] args) throws IOException {
+    public boolean read_robots_txt(String url) {
+        return true;
+    }
+
+    public static void main(String[] args) throws IOException {
         Spider spider = new Spider();
         spider.crawl("https://en.wikipedia.org/wiki/Emmanuel_Macron");
-        }
+    }
+
 }
